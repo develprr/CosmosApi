13ccde7056149be6a4f034178e5d5465c6d8a70a
Heikki Kupiainen <heikki.kupiainen@metamatic.net>
2024-03-17 07:19:54 +0200

    Created initial project structure and minimal code to verify that the application connects Cosmos DB in azure and interacts with MongoDB.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
+
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Cosmos API Connector PoC");
+Connector.Run();
+
+// New instance of CosmosClient class
+
+public record Product(
+    string Id,
+    string Category,
+    string Name,
+    int Quantity,
+    bool Sale
+);
+
+public class Connector
+{
+    public static void Run()
+    {
+        var client = new MongoClient(Environment.GetEnvironmentVariable("COSMOS_CONNECTION_STRING"));
+        var db = client.GetDatabase("adventure");
+
+        var _products = db.GetCollection<Product>("products");
+
+        _products.InsertOne(new Product(
+            Guid.NewGuid().ToString(),
+            "gear-surf-surfboards",
+            "Yamba Surfboard", 
+            12, 
+            false
+        ));
+        var createdProducts = _products.FindSync(p => true);
+        Console.WriteLine(JsonConvert.SerializeObject(createdProducts.ToList(), Formatting.Indented));
+    }
+}
+
